db1eb5815aed543e63dfc8721e8cf4ed72af2b46
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-05-17 11:27:20 +0530

    changes

--- src/admin/adminController.js
+++ src/admin/adminController.js
@@ -4,6 +4,9 @@ const APIFeatures = require("../../utils/apiFeatures");
 const { userModel } = require("../user/user.model");
 const { s3Uploadv2, s3UploadMulti } = require("../../utils/s3");
 const { enquiryModel } = require("../enquiry");
+const { tripModel } = require("../trips/trip.model");
+const truckModel = require("../trucks/truck.model");
+const locationModel = require("../location/location.model");
 
 exports.postSingleImage = catchAsyncError(async (req, res, next) => {
   const file = req.file;
@@ -30,7 +33,7 @@ exports.adminLogin = catchAsyncError(async (req, res, next) => {
   if (!isPasswordMatched)
     return next(new ErrorHandler("Invalid email or password!", 401));
 
-  if (user.role !== 'admin') {
+  if (user.role !== "admin") {
     return next(new ErrorHandler("Only Admin can access the portal.", 401));
   }
 
@@ -40,7 +43,8 @@ exports.adminLogin = catchAsyncError(async (req, res, next) => {
 
 exports.updateAdminProfile = catchAsyncError(async (req, res, next) => {
   console.log("UPDATE ADMIN PROFILE", req.body);
-  const { firstname, lastname, mobile_no, profile_url, password, email } = req.body;
+  const { firstname, lastname, mobile_no, profile_url, password, email } =
+    req.body;
 
   const user = await userModel.findById(req.userId);
   user.email = email;
@@ -89,7 +93,73 @@ exports.calcCharge = catchAsyncError(async (req, res, next) => {
     ttl_milage += end_milage - unload_milage;
   }
 
-  console.log({ ttl_milage, fuel_eff, fuel_price, start_milage, load_milage, unload_milage, end_milage })
+  console.log({
+    ttl_milage,
+    fuel_eff,
+    fuel_price,
+    start_milage,
+    load_milage,
+    unload_milage,
+    end_milage,
+  });
   const price = ttl_milage * fuel_eff * fuel_price;
   res.status(200).json({ price });
-}); 
+});
+
+exports.getDashBoardData = catchAsyncError(async (req, res, next) => {
+  const [userCount, tripCount, truckCount, locationCount] = await Promise.all([
+    userModel.countDocuments(),
+    tripModel.countDocuments(),
+    truckModel.countDocuments(),
+    locationModel.countDocuments(),
+  ]);
+
+  res.status(200).json({
+    success: true,
+    dashBoardData: [
+      { key: "Drivers", value: userCount },
+      { key: "Trips", value: tripCount },
+      { key: "Trucks", value: truckCount },
+      { key: "Locations", value: locationCount },
+    ],
+  });
+});
+
+exports.createUser = catchAsyncError(async (req, res, next) => {
+  const {firstname, lastname, email, password, mobile_no, country_code} =
+    req.body;
+  let location = "";
+  const existingdriver = await userModel.findOne({
+    $or: [{ email }, { mobile_no }],
+  });
+
+  if (existingdriver) {
+    return next(
+      new ErrorHandler("Driver Already exist with this email/mobile", 400)
+    );
+  }
+
+  if (req.file) {
+    const result = await s3Uploadv2(req.file);
+    location = result.Location;
+  }
+
+  const driver = userModel.create({
+    firstname,
+    lastname,
+    email,
+    password,
+    mobile_no,
+    country_code,
+    isRegistered: true,
+    profile_url: location,
+  });
+
+  // await driver.save();
+
+  res.status(201).json({
+    success: true,
+    message: "Driver Created Successfully",
+    driver,
+  });
+});
